61c3dc62239cdc35e96060f4e93fc4bc37cd56cf
Michael Steffens <michael_steffens@posteo.de>
2021-10-07 08:29:27 +0200

    Try __ prefix.

--- lib/NativeAllocator_Prefix/include/NativeAllocator/malloc.h
+++ lib/NativeAllocator_Prefix/include/NativeAllocator/malloc.h
@@ -10,31 +10,31 @@
 
 #include <cstddef>
 
-extern "C" void* _malloc(std::size_t size);
-extern "C" void _free(void* ptr);
-extern "C" void* _calloc(std::size_t nmemb, std::size_t size);
-extern "C" void* _realloc(void* ptr, std::size_t size);
+extern "C" void* __malloc(std::size_t size);
+extern "C" void __free(void* ptr);
+extern "C" void* __calloc(std::size_t nmemb, std::size_t size);
+extern "C" void* __realloc(void* ptr, std::size_t size);
 
 namespace NativeAllocator {
 
 inline void* malloc(std::size_t size) noexcept
 {
-	return _malloc(size);
+	return __malloc(size);
 }
 
 inline void free(void* ptr) noexcept
 {
-	_free(ptr);
+	__free(ptr);
 }
 
 inline void* calloc(std::size_t nmemb, std::size_t size) noexcept
 {
-	return _calloc(nmemb, size);
+	return __calloc(nmemb, size);
 }
 
 inline void* realloc(void* ptr, std::size_t size) noexcept
 {
-	return _realloc(ptr, size);
+	return __realloc(ptr, size);
 }
 
 } // namespace NativeAllocator
